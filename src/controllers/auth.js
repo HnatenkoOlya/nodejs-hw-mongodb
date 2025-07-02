48d862d1d349d6d9c607ac7daf3b5999553ddc79
HnatenkoOlya <gnatenkoolgaig1210@gmail.com>
2025-07-02 19:51:54 +0200

    5/2

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,4 +1,5 @@
-import { registerUser } from '../services/auth.js';
+import { ONE_DAY } from '../constants/index.js';
+import { loginUser, logoutUser, refreshUsersSession, registerUser } from '../services/auth.js';
 
 export const registerUserController = async (req, res) => {
   const user = await registerUser(req.body);
@@ -9,3 +10,60 @@ export const registerUserController = async (req, res) => {
     data: user,
   });
 };
+
+export const loginUserController = async (req, res) => {
+  const session = await loginUser(req.body);
+
+  res.cookie("refreshToken", session.refreshToken, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY),
+  });
+  res.cookie('sessionId', session._id, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY),
+  });
+  res.json({
+    status: 200,
+    message: "Successfully logged in an user!",
+    data: {
+      accessToken: session.accessToken,
+    },
+  });
+};
+
+const setupSession = (res, session) => {
+  res.cookie("refreshToken", session.refreshToken, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY),
+  });
+  res.cookie('sessionId', session._id, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY),
+  });
+};
+
+export const refreshUserSessionController = async (req, res) => {
+  const session = await refreshUsersSession({
+    sessionId:req.cookies.sessionId,
+    refreshToken:req.cookies.refreshToken,
+  });
+  setupSession(res, session);
+  res.json({
+    status: 200,
+    message: "Successfully refreshed a session!",
+    data: {
+      accessToken: session.accessToken,
+    },
+  });
+};
+
+export const logoutUserController = async (req, res) => {
+  if(req.cookies.sessionId) {
+    await logoutUser(req.cookies.sessionId);
+  }
+
+  res.clearCookie("sessionId");
+  res.clearCookie("refreshToken");
+
+  return res.status(204).send();
+}
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -2,11 +2,11 @@ import { ContactsCollection} from '../db/models/contacts.js';
 import {calculatePaginationData} from '../utils/calculatePaginationData.js';
 import {SORT_ORDER} from '../constants/index.js';
 
-export const getAllContacts = async ({ page, perPage, sortOrder = SORT_ORDER.ASC, sortBy, filter = {}, }) => {
+export const getAllContacts = async ({  userId, page, perPage, sortOrder = SORT_ORDER.ASC, sortBy, filter = {}, }) => {
   const limit = perPage;
   const skip = (page - 1) * perPage;
 
-  const contactsQuery = ContactsCollection.find();
+  const contactsQuery = ContactsCollection.find({ userId });
    if (filter.contactType) {
     contactsQuery.where('contactType').eq(filter.contactType);
   }
@@ -25,8 +25,8 @@ export const getAllContacts = async ({ page, perPage, sortOrder = SORT_ORDER.ASC
   };
 };
 
-export const getContactById = async (contactId) => {
-  const contact = await ContactsCollection.findById(contactId);
+export const getContactById = async (contactId, userId) => {
+  const contact = await ContactsCollection.findOne({ _id: contactId, userId });
   return contact;
 };
 
@@ -35,9 +35,9 @@ export const createContact = async (payload) => {
   return contact;
 };
 
-export const updateContact = async (contactId, payload, options = {}) => {
+export const updateContact = async (contactId, userId, payload, options = {}) => {
    const rawResult = await ContactsCollection.findOneAndUpdate(
-    { _id: contactId },
+    { _id: contactId, userId },
     payload,
     {
       new: true,
@@ -54,9 +54,10 @@ export const updateContact = async (contactId, payload, options = {}) => {
   };
 };
 
-export const deleteContact = async (contactId) => {
+export const deleteContact = async (contactId, userId) => {
   const contact = await ContactsCollection.findOneAndDelete({
     _id: contactId,
+    userId,
   });
 
   return contact;
